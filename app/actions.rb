5b422528454e253f1978e1557d4ef1ea19d6964f
Paul Iogna <paul.iogna@gmail.com>
2016-01-25 11:44:42 -0500

    adds feature to post new track

--- app/actions.rb
+++ app/actions.rb
@@ -7,3 +7,23 @@ get '/tracks' do
   @tracks = Track.all
   erb :'tracks/index'
 end
+
+get '/tracks/new' do
+  @track = Track.new
+  erb :'tracks/new'
+end
+
+post '/tracks/new' do
+  url = params[:url] == "" ? nil : params[:url]
+  @track = Track.new(
+    title: params[:title],
+    author: params[:author],
+    url: url,
+    url_text: params[:url_text]
+  )
+  if @track.save
+    redirect '/tracks'
+  else
+    erb :'tracks/new'
+  end
+end
--- app/models/track.rb
+++ app/models/track.rb
@@ -3,9 +3,15 @@ class Track < ActiveRecord::Base
   validates :author, presence: true
   validate :url, :valid_url
 
+  before_create :set_url_text_to_nil_if_url_is_nil
+
   URL_REGEX = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
 
   def valid_url
-    errors.add(:url, "Link must be a valid URL") unless url = URL_REGEX || url.nil?
+    errors.add(:url, "Link must be a valid URL") unless url =~ URL_REGEX || url.nil?
+  end
+
+  def set_url_text_to_nil_if_url_is_nil
+    url_text = nil if url.nil?
   end
 end
